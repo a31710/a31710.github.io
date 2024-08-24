34fccec386b54b48ba90137d67ecc0304828f12d
dinhtien <materkaman576@gmail.com>
2024-08-24 23:11:49 +0700

    new commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Iem iu à!',
+    introDesc: `Anh có điều này muốn hỏi em nhớ phải trả lời thật lòng nhaaa.`,
+    btnIntro: 'Okiii',
+    title: 'Em tha lỗi cho a nhé  ._.',
+    desc: 'Nếu không trả lời mà muốn thoát ra là tha lỗi cho anh nha <3',
+    btnYes: 'Okiiii <3',
+    btnNo: 'Đéo',
+    question:'Heehe Anh bít là em khum giận a mà <3, Thế Iem có iu a hong',
+    btnReply: 'Gửi cho anh iu <3',
+    reply: 'Iu a nhìu lắm',
+    mess: 'Anh bít mà 🥰. Iu em nhiều nhiều 😘😘',
+    messDesc: 'hoy muộn gòi, Chúc em ngủ ngonnnn nha <3',
+    btnAccept: 'Okiiiii lun <3',
+    messLink: 'https://www.google.com/search?q=epl&sca_esv=e7553f361a6275f8&sca_upv=1&sxsrf=ADLYWIIUQ0Ana_sxFmHDtHg1WhkuNuQYdw%3A1724514653042&source=hp&ei=XQHKZpMO29_aug_Gp5uZCQ&iflsig=AL9hbdgAAAAAZsoPbZattZH-RdiWA7QkPmSuY-T7tHkg&ved=0ahUKEwjTsLXa_Y2IAxXbr1YBHcbTJpMQ4dUDCBY&uact=5&oq=epl&gs_lp=Egdnd3Mtd2l6GgIYAiIDZXBsMg8QIxiABBgnGIoFGEYY_QEyEBAuGIAEGLEDGEMYgwEYigUyEBAAGIAEGLEDGEMYgwEYigUyEBAAGIAEGLEDGEMYgwEYigUyEBAAGIAEGLEDGEMYgwEYigUyCxAAGIAEGLEDGIMBMgsQABiABBixAxiDATILEAAYgAQYsQMYgwEyCxAAGIAEGLEDGIMBMg4QABiABBixAxiDARiKBUi9AlAAWHNwAHgAkAEAmAF6oAHEAqoBAzEuMrgBA8gBAPgBAZgCA6ACzwLCAgoQIxiABBgnGIoFwgIKEC4YgAQYJxiKBcICFhAuGIAEGLEDGNEDGEMYgwEYxwEYigXCAgsQLhiABBixAxiDAcICBRAAGIAEwgIFEC4YgATCAhEQLhiABBixAxjRAxiDARjHAcICDRAAGIAEGLEDGEMYigXCAgoQABiABBhDGIoFwgINEAAYgAQYQxiKBRiLA5gDAOIDBRIBMSBAkgcDMC4zoAekKQ&sclient=gws-wiz#sie=m;/g/11w1hz88bp;2;/m/02_tc;dt;fp;1;;;&wptab=si:ACC90nxf9KAEpHgHfAikz_8O8Y5Hh0ixj21IrePIxqwFME2hPrAtHgxFMhMJd22o6cAVUtabSJThEHz0rv3ESLFKsWwB_IDhEVyMUaCOxXoM7m8kO3nHJw6AtC9ZAcPdLIsC6eXmZBxJsSxF1qJkU2uRUly-_d__Fz_j9rwBYTzvTfgkZtuuobI%3D' //link mess của các bạn. VD: https://m.me/nam.nodemy
+}
